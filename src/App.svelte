2810f1befe54e62fd74341fc4be40aa0654d7761
Riza Fahmi <rizafahmi@gmail.com>
2020-06-01 16:54:41 +0700

    Siklus hidup komponen Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,14 @@
 <script>
+  import { onMount } from 'svelte';
   import Navigation from './components/Navigation.svelte';
   import CharityList from './components/CharityList.svelte';
 
-  const charities = [
-    {
-      id: 0,
-      title: 'Edukasi untuk Semua',
-      description: 'Menyediakan sarana edukasi untuk semua anak Indonesia.',
-      img:
-        'https://cdn.pixabay.com/photo/2017/07/30/16/22/kid-2554803_1280.jpg',
-      raised: 0,
-      goal: 10000000
-    },
-    {
-      id: 1,
-      title: 'Komputer untuk Anak Indonesia',
-      description: 'Menyediakan perangkat komputer untuk semua anak Indonesia.',
-      img:
-        'https://cdn.pixabay.com/photo/2016/11/14/03/14/children-1822471_1280.jpg',
-      raised: 0,
-      goal: 10000000000
-    }
-  ];
+  let charities = [];
+
+  onMount(async function() {
+    const res = await fetch(`https://charity-api-bwa.herokuapp.com/charities`);
+    charities = await res.json();
+  });
 </script>
 
 <style>
