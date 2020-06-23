581956fd33a28f6bd22632ff418ef26968c220a3
Riza Fahmi <rizafahmi@gmail.com>
2020-06-23 09:28:40 +0700

    install npm package

--- functions/payment.js
+++ functions/payment.js
@@ -1,6 +1,35 @@
 exports.handler = function(event, context, callback) {
+  const Midtrans = require('midtrans-client');
+
+  const snap = new Midtrans.Snap({
+    isProduction: false,
+    serverKey: process.env.MIDTRANS_SERVER_KEY,
+    clientKey: process.env.MIDTRANS_CLIENT_KEY
+  });
+
+  const parameter = {
+    transaction_details: {
+      order_id: '124',
+      gross_amount: 20000
+    },
+    credit_card: {
+      secure: true
+    }
+  };
+
+  snap
+    .createTransaction(parameter)
+    .then((transaction) => {
+      const { token } = transaction;
+      console.log(`Token: ${token}`);
+
+      const redirectUrl = transaction.redirect_url;
+      console.log(`URL: ${redirectUrl}`);
+    })
+    .catch((e) => console.error(`error: ${e.message}`));
+
   callback(null, {
     statusCode: 200,
-    body: 'This is netlify fn'
+    body: redirectUrl
   });
 };
